c1b53888cc4fe33a8e33680465aea65566f230a5
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-04-06 13:56:36 -0400

    completes assignment

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -1,90 +1,106 @@
 // require
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../data/helpers/actionModel');
+const db = require("../data/helpers/actionModel.js");
 
-// handles routes that start with: /api/users
+// handles routes that start with: /api/actions
 
-router.get('/', (req, res) => {
-    // get data
-    db.get()
-      // send the data
-      .then(users => {
-        res.json(users);
-      })
-      // send error if there is one
-      .catch(error => {
-        res.status(500).json(err);
-      });
-  });
-  
-  router.get("/:id", (req, res) => {
+router.get("/", (req, res) => {
+  // get data
+  db
+    .get()
+    // send the data
+    .then(actions => {
+      res.json(actions);
+    })
+    // send error if there is one
+    .catch(error => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db
+    .get(req.params.id)
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post("/", (req, res) => {
+  const action = req.body;
+  if (!action.project_id || !action.description) {
+    res.status(400).json({ error: "project_id AND description are Required" });
+  } else if (typeof action.project_id !== "string") {
+    res.status(400).json({ error: "project_id must be a typeof string" });
+  } else if (typeof action.description !== "string") {
+    res.status(400).json({ error: "description must be a typeof string" });
+  } else if (action.project_id.length > 128 || action.description.length > 128) {
+    res.status(400).json({ error: "max length is 128 characters" });
+  } else {
+  db
+    .insert(action)
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The actions information could not be retrieved." });
+    });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  //const action = req;
+  db
+    .remove(id)
+    .then(response => {
+      res.status(200).json(action);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const action = req.body;
+  if (action.project_id === undefined && action.description === undefined) {
+    res.status(400).json({ error: "project_id or description are required" });
+  } else if (typeof action.project_id !== "string") {
+    res.status(400).json({ error: "project_id is required" });
+  } else if (
+    action.project_id.length > 128 ||
+    action.description.length > 128
+  ) {
+    res.status(400).json({ error: "The Max length is 128 characters" });
+  } else {
     db
-      .get(req.params.id)
-      .then(users => {
-        res.json(users);
+      .update(id, action)
+      .then(count => {
+        if (count > 0) {
+          db
+            .get(id)
+            .then(action => {
+              res.status(200).json(action);
+            })
+            .catch(error => {
+              res.status(400).json(error);
+            });
+        } else {
+          res.status(404).json({ errorMessage: "action not found" });
+        }
       })
       .catch(error => {
         res.status(500).json(error);
       });
-  });
-    
-  
-  router.post('/', (req, res) => {
-      const user = req.body;
-      db.insert(user)
-      .then(response => {
-          res.status(201).json(response)
-      })
-      .catch(error => {
-          res.status(500).json({ error: "The users information could not be retrieved." })
-      })
-  })
-  
-  router.delete('/:id', (req, res) => {
-      const { id } = req.params;
-      //const user = req;
-      db.remove(id)
-          .then(response => {
-              res.status(200).json(user);
-          })
-          .catch(error => {
-              res.status(500).json(error);
-          })
-  
-     
-  })
-  
-  router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const user = req.body;
-    if (!user.name) {
-      res.status(400).json({ error: 'Name is Required' });
-    } else if (user.name.length > 128) {
-      res.status(400).json({ error: 'The Max length is 128 characters' });
-    } else {
-      db
-        .update(id, user)
-        .then(count => {
-          if (count > 0) {
-            db
-              .get(id)
-              .then(user => {
-                res.status(200).json(user);
-              })
-              .catch(error => {
-                res.status(400).json(error);
-              });
-          } else {
-            res.status(404).json({ errorMessage: 'User not found' });
-          }
-        })
-        .catch(error => {
-          res.status(500).json(error);
-        });
-    }
-  });
-  
-  module.exports = router;
-  
+  }
+});
+
+module.exports = router;
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -1,90 +1,105 @@
 // require
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../data/helpers/projectModel');
+const db = require("../data/helpers/projectModel.js");
 
-// handles routes that start with: /api/users
+// handles routes that start with: /api/projects
 
-router.get('/', (req, res) => {
-    // get data
-    db.get()
-      // send the data
-      .then(users => {
-        res.json(users);
-      })
-      // send error if there is one
-      .catch(error => {
-        res.status(500).json(err);
-      });
-  });
-  
-  router.get("/:id", (req, res) => {
+router.get("/", (req, res) => {
+  // get data
+  db
+    .get()
+    // send the data
+    .then(projects => {
+      res.json(projects);
+    })
+    // send error if there is one
+    .catch(error => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db
+    .get(req.params.id)
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post("/", (req, res) => {
+  const project = req.body;
+  if (!project.name || !project.description) {
+    res.status(400).json({ error: "name AND description are Required" });
+  } else if (typeof project.name !== "string") {
+    res.status(400).json({ error: "name must be a typeof string" });
+  } else if (typeof project.description !== "string") {
+    res.status(400).json({ error: "description must be a typeof string" });
+  } else if (project.name.length > 128 || project.description.length > 128) {
+    res.status(400).json({ error: "max length is 128 characters" });
+  } else {
     db
-      .get(req.params.id)
-      .then(users => {
-        res.json(users);
+      .insert(project)
+      .then(response => {
+        res.status(201).json(response);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ error: "The projects information could not be retrieved." });
+      });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  //const project = req;
+  db
+    .remove(id)
+    .then(response => {
+      res.status(200).json(project);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const project = req.body;
+  if (project.name === undefined && project.description === undefined) {
+    res.status(400).json({ error: "name or description is required" });
+  } else if (typeof project.name !== "string") {
+    res.status(400).json({ error: "name must be a typeof string" });
+  } else if (typeof project.description !== "string") {
+    res.status(400).json({ error: "description must be a typeof string" });
+  } else if (project.name.length > 128 || project.description.length > 128) {
+    res.status(400).json({ error: "The Max length is 128 characters" });
+  } else {
+    db
+      .update(id, project)
+      .then(count => {
+        if (count > 0) {
+          db
+            .get(id)
+            .then(project => {
+              res.status(200).json(project);
+            })
+            .catch(error => {
+              res.status(400).json(error);
+            });
+        } else {
+          res.status(404).json({ errorMessage: "project not found" });
+        }
       })
       .catch(error => {
         res.status(500).json(error);
       });
-  });
-    
-  
-  router.post('/', (req, res) => {
-      const user = req.body;
-      db.insert(user)
-      .then(response => {
-          res.status(201).json(response)
-      })
-      .catch(error => {
-          res.status(500).json({ error: "The users information could not be retrieved." })
-      })
-  })
-  
-  router.delete('/:id', (req, res) => {
-      const { id } = req.params;
-      //const user = req;
-      db.remove(id)
-          .then(response => {
-              res.status(200).json(user);
-          })
-          .catch(error => {
-              res.status(500).json(error);
-          })
-  
-     
-  })
-  
-  router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const user = req.body;
-    if (!user.name) {
-      res.status(400).json({ error: 'Name is Required' });
-    } else if (user.name.length > 128) {
-      res.status(400).json({ error: 'The Max length is 128 characters' });
-    } else {
-      db
-        .update(id, user)
-        .then(count => {
-          if (count > 0) {
-            db
-              .get(id)
-              .then(user => {
-                res.status(200).json(user);
-              })
-              .catch(error => {
-                res.status(400).json(error);
-              });
-          } else {
-            res.status(404).json({ errorMessage: 'User not found' });
-          }
-        })
-        .catch(error => {
-          res.status(500).json(error);
-        });
-    }
-  });
-  
-  module.exports = router;
-  
+  }
+});
+
+module.exports = router;
