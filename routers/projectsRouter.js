3773f7552cf66b7acaa1a470c4faf255efdf6964
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-04-06 18:30:32 -0400

    attempt at switch case refactor, unable to complete

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -72,18 +72,17 @@ router.put("/:id", (req, res) => {
   const { id } = req.params;
   const project = req.body;
   console.log(req.body)
-  // switch (project.name) {
-  //   case !project.name:
-  //     res.status(400).json({ error: "name is required" });
-  //     break;
-  //   case typeof project.name !== 'string':
-  //     res.status(400).json({ error: "name must be a typeof string" });
-  //     break;
-  //   case (project.name.length > 128):
-  //     res.status(400).json({ error: "The Max length is 128 characters" });
-  //     break;
-  //   default:
-  //     break;
+  switch (project.name) {
+    case undefined:
+      res.status(400).json({ error: "name is required" });
+    case (Number(project.name)): // can't use bangers in switch statements???
+      res.status(400).json({ error: "name must be a typeof string" });
+      break;
+    // Not sure how to implement both project.name and project.description base cases when the default needs to
+    // the same function...
+    default:
+
+      // break;
   // }
   // switch (project.description) {
   //   case !project.description:
@@ -92,25 +91,24 @@ router.put("/:id", (req, res) => {
   //   case typeof project.description !== 'string':
   //     res.status(400).json({ error: "description must be a typeof string" });
   //     break;
-  //   // case project.description.length > 128:
-  //   //   res.status(400).json({ error: "The Max length is 128 characters" });
-  //   //   break;
-  //   default:
+  //   case project.description.length > 128:
+  //     res.status(400).json({ error: "The Max length is 128 characters" });
   //     break;
-  // }
+  //   default:
+  
 
   // if/else approach:
-  if (project.name === undefined && project.description === undefined) {
-    res.status(400).json({ error: "name or description is required" });
-  } else if (project.name && typeof project.name !== "string") {
-      res.status(400).json({ error: "name must be a typeof string" });
-  } else if (project.name && project.name.length > 128) {
-      res.status(400).json({ error: "The Max length is 128 characters" });
-  } else if (project.description && typeof project.description !== "string") {
-      res.status(400).json({ error: "description must be a typeof string" });
-  } else if (project.description && project.description.length > 128) {
-      res.status(400).json({ error: "The Max length is 128 characters" });
-  } else {
+  // if (project.name === undefined && project.description === undefined) {
+  //   res.status(400).json({ error: "name or description is required" });
+  // } else if (project.name && typeof project.name !== "string") {
+  //     res.status(400).json({ error: "name must be a typeof string" });
+  // } else if (project.name && project.name.length > 128) {
+  //     res.status(400).json({ error: "The Max length is 128 characters" });
+  // } else if (project.description && typeof project.description !== "string") {
+  //     res.status(400).json({ error: "description must be a typeof string" });
+  // } else if (project.description && project.description.length > 128) {
+  //     res.status(400).json({ error: "The Max length is 128 characters" });
+  // } else {
     db
     .update(id, project)
     .then(response => {
@@ -131,31 +129,5 @@ router.put("/:id", (req, res) => {
     })
   }
 })
-
-// BROKEN
-
-// db
-// .update(id, project)
-// .then(count => {
-//   if (count > 0) {
-//     db
-//       .get(id)
-//       .then(project => {
-//         res.status(200).json(project);
-//       })
-//       .catch(error => {
-//         res.status(400).json(error);
-//       });
-//   } else {
-//     res.status(404).json({ errorMessage: "project not found" });
-//   }
-// })
-// .catch(error => {
-//   res.status(500).json(error);
-// });
-// // }
-// });
-
-
 
 module.exports = router;
